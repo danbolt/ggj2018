1e4f733f23cfeadb462f21043463611a8ada53e1
Jason Estey <esteyj@yahoo.com>
2018-01-26 21:55:15 -0800

    update player to use physics

--- src/actors/player.js
+++ src/actors/player.js
@@ -1,21 +1,22 @@
 var Player = function (game, x, y) {
   Phaser.Sprite.call(this, game, x, y, 'coloured_squares', 4);
   this.anchor.set(0.5, 0.5);
-  this.speed = 0.2;
+  this.game.physics.enable(this, Phaser.Physics.ARCADE);
 };
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 Player.prototype.update = function () {
+  this.body.velocity.set(0, 0);
   if (this.game.input.keyboard.isDown(Phaser.KeyCode.UP)) {
-    this.y -= this.game.time.elapsed * this.speed;
+    this.body.velocity.y = -100;
   }
   if (this.game.input.keyboard.isDown(Phaser.KeyCode.DOWN)) {
-    this.y += this.game.time.elapsed * this.speed;
+    this.body.velocity.y = 100;
   }
   if (this.game.input.keyboard.isDown(Phaser.KeyCode.LEFT)) {
-    this.x -= this.game.time.elapsed * this.speed;
+    this.body.velocity.x = -100;
   }
   if (this.game.input.keyboard.isDown(Phaser.KeyCode.RIGHT)) {
-    this.x += this.game.time.elapsed * this.speed;
+    this.body.velocity.x = 100;
   }
 };
