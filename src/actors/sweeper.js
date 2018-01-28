87f257a6f157b9ce2a5ebaa166924a31a11bbf47
Daniel Savage <dansvg@gmail.com>
2018-01-27 20:33:55 -0800

    Sweeper animation

--- src/actors/sweeper.js
+++ src/actors/sweeper.js
@@ -1,21 +1,29 @@
 var Sweeper = function (game, player, spawnData) {
-  Phaser.Sprite.call(this, game, spawnData.x, spawnData.y, 'coloured_squares', 7);
+  Phaser.Sprite.call(this, game, spawnData.x, spawnData.y, 'coloured_squares', 28);
   this.game.physics.enable(this, Phaser.Physics.ARCADE);
-  this.startX = spawnData.x;
-  this.destinationX = spawnData.properties.destinationX || this.startX;
-  this.speed = spawnData.properties.speed || 1;
+  this.speed = spawnData.properties.speed || 50;
+
+  this.body.velocity.x = this.speed;
+
+  // cosmetic scaling
+  var actualSprite = this.game.add.sprite(8, 8, 'coloured_squares', 8);
+  actualSprite.anchor.set(0.5);
+  this.addChild(actualSprite);
+  actualSprite.animations.add('go', [25, 26, 27, 26], 8, true);
+  actualSprite.animations.play('go');
+  var t = this.game.add.tween(actualSprite.scale);
+  t.to( {x: [1.4, 1, 0.6, 1], y: [0.6, 1, 1.4, 1]}, 750, Phaser.Easing.Cubic.InOut, false, undefined, -1);
+  t.start();
 };
 Sweeper.prototype = Object.create(Phaser.Sprite.prototype);
 Sweeper.prototype.constructor = Sweeper;
 Sweeper.prototype.update = function () {
-  var dx = this.destinationX - this.startX;
-  var t = 1 - (1 + Math.cos(this.game.time.totalElapsedSeconds() * this.speed)) * 0.5;
-  this.x = this.startX + dx * t;
-
-  if (this.body) {
-    this.body.velocity.x = 0;
-    this.body.velocity.y = 0;
+  if (this.body.blocked.right) {
+    this.body.velocity.x = this.speed * -1;
+  } else if (this.body.blocked.left) {
+    this.body.velocity.x = this.speed;
   }
+  this.body.velocity.y = 0;
 };
 Sweeper.prototype.onCollision = function (player) {
   player.kill();
