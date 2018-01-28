9e48c077649e5a36b2cdab3fb23aaa9c014b135c
Daniel Savage <dansvg@gmail.com>
2018-01-27 21:31:28 -0800

    Level exit animation

--- src/actors/levelexit.js
+++ src/actors/levelexit.js
@@ -1,16 +1,30 @@
 var LevelExit = function(game, x, y) {
-  Phaser.Sprite.call(this, game, x, y, 'coloured_squares', 1);
+  Phaser.Sprite.call(this, game, x, y, 'coloured_squares', 40);
   this.game.physics.enable(this, Phaser.Physics.ARCADE);
 
+  this.animations.add('open', [40, 41, 42, 43], 10, false);
+
   this.lock();
+  this.tween = null;
 };
 LevelExit.prototype = Object.create(Phaser.Sprite.prototype);
 LevelExit.prototype.constructor = LevelExit;
 LevelExit.prototype.lock = function () {
   this.data.unlocked = false;
-  this.tint = 0x000000;
+  this.frame = 40;
+
+  if (this.tween) {
+    this.tween.stop();
+    this.tween = null;
+  }
 };
 LevelExit.prototype.unlock = function () {
   this.data.unlocked = true;
-  this.tint = 0xFFFFFF;
+  this.animations.play('open');
+
+  if (this.tween === null) {
+    this.tween = this.game.add.tween(this);
+    this.tween.to( { tint: [0xDDDDDD, 0xFFFFFF] }, 500, Phaser.Easing.Cubic.InOut, true, 0, -1);
+    this.tween.start();
+  }
 };
