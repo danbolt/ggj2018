a0dd6ff88659ea4b1873c2d44a112bdadde3018c
Daniel Savage <dansvg@gmail.com>
2018-01-27 20:53:58 -0800

    Animate walker with 'thump' motion

--- src/actors/chaser.js
+++ src/actors/chaser.js
@@ -1,15 +1,23 @@
 var Chaser = function (game, player, spawnData) {
-  Phaser.Sprite.call(this, game, spawnData.x, spawnData.y, 'coloured_squares', 7);
+  Phaser.Sprite.call(this, game, spawnData.x, spawnData.y, 'coloured_squares', 28);
   this.game.physics.enable(this, Phaser.Physics.ARCADE);
   this.player = player;
   this.moveSpeed = spawnData.properties.speed || 25;
 
   this.onCollision = true;
 
-  this.animations.add('down', [32, 33], 4, true);
-  this.animations.add('up', [38, 39], 4, true);
-  this.animations.add('right', [34, 35], 4, true);
-  this.animations.add('left', [36, 37], 4, true);
+  // cosmetic scaling
+  var actualSprite = this.game.add.sprite(8, 16, 'coloured_squares', 8);
+  actualSprite.anchor.set(0.5, 1);
+  this.addChild(actualSprite);
+  actualSprite.animations.add('down', [32, 33], 4, true);
+  actualSprite.animations.add('up', [38, 39], 4, true);
+  actualSprite.animations.add('right', [34, 35], 4, true);
+  actualSprite.animations.add('left', [36, 37], 4, true);
+  this.animations = actualSprite.animations;
+  var t = this.game.add.tween(actualSprite.scale);
+  t.to( {y: [1.4, 1]}, 250, Phaser.Easing.Cubic.InOut, false, undefined, -1);
+  t.start();
 };
 Chaser.prototype = Object.create(Phaser.Sprite.prototype);
 Chaser.prototype.constructor = Chaser;
