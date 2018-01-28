9586e5faa856920aa4a5d82070d90ddb92edd55b
Jason Estey <esteyj@yahoo.com>
2018-01-27 18:41:08 -0800

    sweepers start at their spawn position

--- src/actors/sweeper.js
+++ src/actors/sweeper.js
@@ -9,7 +9,7 @@ Sweeper.prototype = Object.create(Phaser.Sprite.prototype);
 Sweeper.prototype.constructor = Sweeper;
 Sweeper.prototype.update = function () {
   var dx = this.destinationX - this.startX;
-  var t = (1 + Math.cos(this.game.time.totalElapsedSeconds() * this.speed)) * 0.5;
+  var t = 1 - (1 + Math.cos(this.game.time.totalElapsedSeconds() * this.speed)) * 0.5;
   this.x = this.startX + dx * t;
 
   if (this.body) {
